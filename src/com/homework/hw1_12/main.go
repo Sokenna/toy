1bc5a083e358dbd3f3ecc04b8987359b202cca96
Sokenna <44802390+Sokenna@users.noreply.github.com>
2026-04-12 23:21:03 +0800

    just do do do

--- src/com/homework/hw1_12/main.go
+++ src/com/homework/hw1_12/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/gif"
@@ -9,44 +8,32 @@ import (
 	"math"
 	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 var palette = []color.Color{color.White, color.Black}
 
 const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
+	whileIndex = 0
+	blackIndex = 1
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/showGif", showGif)
+	http.HandleFunc("/showGif", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
-
 }
-
-// handler echoes the Path component of the requested URL.
-
-// counter echoes the number of calls so far.
-
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header [%q]  =  %q\n", k, v)
-	}
-	fmt.Fprintf(w, "Host = %q \n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr = %q \n", r.RemoteAddr)
 	if err := r.ParseForm(); err != nil {
 		log.Print(err)
 	}
-	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q]=[%q]\n", k, v)
-	}
-}
 
-func showGif(w http.ResponseWriter, r *http.Request) {
+	cycle, err := strconv.Atoi(r.Form.Get("cycle"))
+	if err != nil {
+		return
+	}
+
 	const (
-		cycles  = 5
+		//cycles  = cycle
 		res     = 0.001
 		size    = 100
 		nframes = 64
@@ -58,7 +45,7 @@ func showGif(w http.ResponseWriter, r *http.Request) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < float64(cycle)*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
@@ -69,5 +56,4 @@ func showGif(w http.ResponseWriter, r *http.Request) {
 		anim.Image = append(anim.Image, img)
 	}
 	gif.EncodeAll(w, &anim)
-
 }
