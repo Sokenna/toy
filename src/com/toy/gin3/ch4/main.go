2287a6aa3140a03576727316af523f2b3814b7ce
Sokenna <44802390+Sokenna@users.noreply.github.com>
2026-04-19 16:42:45 +0800

    just do do do

---
+++ src/com/toy/gin3/ch4/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/someJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "C语言",
+			"tag":  "<br>",
+		}
+		c.AsciiJSON(http.StatusOK, data)
+	})
+	r.Run(":8000")
+}
